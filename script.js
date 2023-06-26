6717db070cce54ef5b5f64c429117ca298bbc92f
Hiren Patel <hiren8891@gmail.com>
2023-06-26 18:17:18 +0530

    update in script

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ const spinner = ora('This is the post-init script');
 const PROJECT_PATH = process.cwd();
 console.log('PROJECT_PATH local', PROJECT_PATH);
 const PACKAGE_JSON_PATH = `${PROJECT_PATH}/${path.basename(process.cwd())}/App.tsx`;
-const PLIST_PATH = `${PROJECT_PATH}/${path.basename(process.cwd())}/ios/${path.basename(process.cwd())}/info.plist`;
-const STRING_XML_PATH = `${PROJECT_PATH}/${path.basename(process.cwd())}/android/app/src/main/res/values/strings.xml`;
+const PLIST_PATH = `${PROJECT_PATH}/ios/${path.basename(process.cwd())}/info.plist`;
+const STRING_XML_PATH = `${PROJECT_PATH}/android/app/src/main/res/values/strings.xml`;
 
 console.log('PACKAGE_JSON_PATH local', PACKAGE_JSON_PATH);
 
